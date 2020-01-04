ccd5542fb33f2572a70ddbca4a49cd471606e08e
simoneboerci <boercisimone@gmail.com>
2020-01-04 17:18:23 +0100

    ./asteroid <script>

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Asteroid : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    [Header("Variables")]
+    public float movementSpeed;
+    public float rotationSpeed;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddForce(Vector3.left * movementSpeed * Time.fixedDeltaTime);
+        _rb.AddTorque(Vector3.up * rotationSpeed * Time.fixedDeltaTime);
+    }
+}
